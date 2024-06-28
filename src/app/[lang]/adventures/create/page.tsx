f7cd748da98c35c64265a88603c74c9eaa852684
Rene Kremer <renejon90@googlemail.com>
2024-06-29 00:07:27 +0200

    feat(#25): Add NoSystemDialog to adventure create page and adventure gallery page
    closes #25

--- src/app/[lang]/adventures/create/page.tsx
+++ src/app/[lang]/adventures/create/page.tsx
@@ -1,17 +1,20 @@
 import { AdventureForm } from "@features/adventures";
 import { tagRepository } from "@features/tags";
 import { genreRepository } from "@features/genres";
-import { systemRepository } from "@features/systems";
+import { NoSystemsDialog, systemRepository } from "@features/systems";
 
 export default async function AdventureCreatePage() {
   const tags = await tagRepository.list();
   const genres = await genreRepository.list();
   const systems = await systemRepository.list();
-  return (
+  const doSystemsExist = systems.length > 0;
+  return doSystemsExist ? (
     <AdventureForm
       tagOptions={tags}
       genreOptions={genres}
       systemOptions={systems}
     />
+  ) : (
+    <NoSystemsDialog />
   );
 }
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -5,10 +5,15 @@ import {
   loadAdventures,
 } from "@features/adventures";
 import { Box } from "@mui/material";
+import { NoSystemsDialog, systemRepository } from "@features/systems";
 
 export default async function AdventureCollectionPage() {
   await startSeeding();
   const adventures = await loadAdventures();
+  const systems = await systemRepository.list();
+  const doSystemsExist = systems.length > 0;
+
+  if (!doSystemsExist) return <NoSystemsDialog />;
   return adventures.length > 0 ? (
     <Box sx={{ marginLeft: 2, marginRight: 2 }}>
       <AdventureCardGallery adventures={adventures} />
